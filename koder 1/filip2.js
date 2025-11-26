0f84b36e25819b160a72812a9acda94ec3db925c
filipcirkovic-art <filip.cirkovic@gritacademy.se>
2025-11-26 22:55:09 +0100

    uppift 2

---
+++ koder 1/filip2.js
@@ -0,0 +1,17 @@
+let age= document.getElementById("ageInput").ValueASNumber
+
+let vip =  document.getElementById("vipinput").checked
+
+
+
+if(vip == true){
+ console.log(say "welcome my best vip customer:)") 
+} else if (age >= 18){
+alert("acess granted")
+ } else {
+    alert("acess denied")
+}
+
+
+
+document.getElementById("enter").onclick = enter
---
+++ koder 1/scripts/filip.js
@@ -0,0 +1,10 @@
+
+let foo = 0
+foo = 0
+
+let bar = 0
+
+bar = 0
+
+
+console.log (789)
